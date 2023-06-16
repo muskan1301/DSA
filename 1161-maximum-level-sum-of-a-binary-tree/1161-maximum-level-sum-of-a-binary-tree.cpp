09eff920545f40eb60713593d51611a2244bd83a
muskan@1301 <73812098+muskan1301@users.noreply.github.com>
2023-06-16 11:24:49 +0530

    Time: 213 ms (38.65%), Space: 104.8 MB (95.42%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,34 +11,29 @@
  */
 class Solution {
 public:
+    map<int,int>mp;
+    //inorder 
+    void DFS(TreeNode*root,int level){
+        if(root==NULL){
+            return;
+        }
+        mp[level] += root->val;
+        DFS(root->left,level+1);
+        DFS(root->right,level+1);
+    }   
     int maxLevelSum(TreeNode* root) {
-        int maxSum = INT_MIN;
-        int resultLevel = 0;
-        
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        int currLevel=1;
-        while(!q.empty()){
-            int n = q.size();
-            int sum=0;
+        mp.clear();
+        DFS(root,1);
+         int maxSum = INT_MIN;
+        int  resultLevel = 0;
+        for(auto &it:mp){
+            int level = it.first;
+            int sum = it.second;
             
-            while(n--){
-                TreeNode*temp = q.front();
-                q.pop();
-                sum += temp->val;
-                if(temp->left){
-                    q.push(temp->left);
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                }
-            }
             if(sum>maxSum){
-                maxSum = sum;
-                resultLevel = currLevel;
+                maxSum= sum;
+                resultLevel = level;
             }
-            currLevel++;
         }
         return resultLevel;
     }
