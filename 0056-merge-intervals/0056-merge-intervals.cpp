bfb29327b1e725406dd4ebf93bc73390ffce74a4
muskan@1301 <73812098+muskan1301@users.noreply.github.com>
2023-07-12 22:02:57 +0530

    Time: 36 ms (91.50%), Space: 19 MB (74.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,19 +4,14 @@ public:
         vector<vector<int>>ans;
         sort(intervals.begin(),intervals.end());
         for(int i=0;i<intervals.size();i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]) continue;
-            for(int j=i+1;j<intervals.size();j++){
-                if(intervals[j][0]<=end){
-                    end = max(end,intervals[j][1]);
-                }
-                else{
-                    break;
+            if(ans.empty() ||  intervals[i][0]> ans.back()[1])
+                ans.push_back(intervals[i]);
+            else{
+                    ans.back()[1] = max(intervals[i][1],ans.back()[1]);
                 }
             }
-            ans.push_back({start,end});
-        }
         return ans;       
     }
 };
+
+//TC -> O(nlogn)+O(n)
