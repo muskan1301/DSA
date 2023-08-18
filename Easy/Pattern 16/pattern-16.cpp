4e66f304faa807b152c6cae83e7a17b1f6d59e0d
muskan@1301 <73812098+muskan1301@users.noreply.github.com>
2023-08-18 23:27:53 +0530

    Updated Solution - GfG to GitHub

--- Easy/Pattern 16/pattern-16.cpp
+++ Easy/Pattern 16/pattern-16.cpp
@@ -7,12 +7,16 @@ using namespace std;
 class Solution {
   public:
     void printTriangle(int n) {
-        for(char ch ='A';ch<'A'+n;ch++){
-            for(char ch1='A';ch1<=ch;ch1++){
+        for(int i=0;i<n;i++){
+            char ch = 'A'+i;
+            for(int j=0;j<=i;j++){
                 cout<<ch;
             }
             cout<<endl;
         }
+
+
+
     }
 };
 
