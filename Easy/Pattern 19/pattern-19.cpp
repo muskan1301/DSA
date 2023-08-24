80880a123b43ad6deb4de9a44abfb079d781b0ea
muskan@1301 <73812098+muskan1301@users.noreply.github.com>
2023-08-24 22:41:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 19/pattern-19.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=2*n;i++){
+            for(int j=1;j<=2*n;j++){
+                if(i<=n){
+                    if(j<=n-i+1||j>n+i-1){
+                        cout<<"*";
+                    }
+                    else{
+                        cout<<" ";
+                    }
+                   
+                }
+                else{
+                    if(j<=i-n ||j>3*n-i)
+                        cout<<"*";
+                    else{
+                        cout<<" ";
+                    }    
+                }
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
